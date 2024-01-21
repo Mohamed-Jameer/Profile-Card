98b3de8af0ca4f3d7551afbf4247ee2dc60f2dd5
Mohamed Jameer N <110616353+Mohamed-Jameer@users.noreply.github.com>
2024-01-21 12:52:41 +0530

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,60 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="jonas.jpeg" alt="jameer" />;
+}
+function Intro() {
+  return (
+    <div>
+      <h1>MOHAMED JAMEER</h1>
+      <p>
+        Full-stack web developer and teacher at Udemy. When not coding or
+        preparing a course, I like to play board games, to cook (and eat), or to
+        just enjoy the Portuguese sun at the beach.
+      </p>
+    </div>
+  );
+}
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="😍" color="red" />
+      <Skill skill="HTMl & CSS" emoji="👍" color="orange" />
+      <Skill skill="Java" emoji="😎" color="blue" />
+      <Skill skill="Java Script" emoji="👌" color="yellow" />
+    </div>
+  );
+}
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
